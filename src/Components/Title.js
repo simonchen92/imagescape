68bce48d3d8310d94354b4f43254767724890f66
simonchen92 <simon.k.chen92@gmail.com>
2021-04-29 14:33:57 -0400

    feat(app-header): clean up unused files from create react app & create component for app title

---
+++ src/Components/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <div className="title">
+      <h1>ImageScape</h1>
+      <h2>Your Pictures</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+  );
+}
