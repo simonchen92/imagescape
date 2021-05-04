4bd3bf2996a6adace48ee88942e3194f7896dcf1
Simon Chen <46136260+simonchen92@users.noreply.github.com>
2021-05-04 16:27:13 -0400

    Revert "Revert "Feat/upload form/create upload component (#3)" (#4)" (#5)
    This reverts commit 02d0b02e2036712216a102c5b13bad69795c0720.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./Header.scss";
 
-export default function Title() {
+const Title = () => {
   return (
     <div className="title">
       <h1>ImageScape</h1>
@@ -10,4 +10,6 @@ export default function Title() {
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
   );
-}
+};
+
+export default Title;
